7bf9f3f6ddf8a008e03058dd47caf14f3aa5edad
Oleksandr Chervonniy <aschervonniy@gmail.com>
2022-10-26 17:13:37 +0200

    fixed logic bux + add animation styles

--- script.js
+++ script.js
@@ -2,31 +2,46 @@
 
 // Create a random number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.question').textContent = secretNumber;
-
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(typeof guessingNumber);
 
+  // No input value
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Input some number';
+
+    // Player is won
   } else if (guessingNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'You are right!';
+    document.querySelector('.question').textContent = secretNumber;
+    document.querySelector('body').classList.add('active');
+    document.querySelector('.question').classList.add('active');
+
+    // To bigger number
   } else if (guessingNumber > secretNumber) {
     // Add logic when score is 0
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector('.guess-message').textContent = 'To much (◕‿◕)';
       score--;
       document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent =
+        'Game over - start again';
     }
+
+    // To lower number
   } else if (guessingNumber < secretNumber) {
     // Add logic when score is 0
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector('.guess-message').textContent = 'To small number';
       score--;
       document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent =
+        'Game over - start again';
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
